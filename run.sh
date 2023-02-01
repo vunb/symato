1b6758a21068d78de9c1123c588ab067df9617da
Tien Dung <dungtn@gmail.com>
2023-02-01 15:18:59 +0700

    ./run.sh to run the RWKV-4-Pile-169M model

---
+++ run.sh
@@ -0,0 +1,3 @@
+[ -f 20B_tokenizer.json ] || wget https://raw.githubusercontent.com/BlinkDL/RWKV-LM/main/RWKV-v4neo/20B_tokenizer.json
+[ -f RWKV-4-Pile-169M-20220807-8023.pth ] || wget https://huggingface.co/BlinkDL/rwkv-4-pile-169m/resolve/main/RWKV-4-Pile-169M-20220807-8023.pth
+python3 run.py
