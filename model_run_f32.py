b67c944327a667a249fdc0e31e51428c19b50fe2
Tien Dung <dungtn@gmail.com>
2023-02-02 17:30:28 +0700

    cleanup

--- model_run_f32.py
+++ model_run_f32.py
@@ -1,71 +1,60 @@
+import gc
 import types
 import torch
-import math, os, gc
 from torch.nn import functional as F
-import torch.nn as nn
-from typing import List, Dict
 
-MyModule = nn.Module
-def __nop(ob): return ob
-MyFunction = __nop
-
-# try torch jit --> faster for fp32, slower for fp16 (why?)
-RWKV_JIT_ON = os.environ.get("RWKV_JIT_ON", "1") # if not set, enable by default
-if  RWKV_JIT_ON == "1":
-    MyModule = torch.jit.ScriptModule
-    MyFunction = torch.jit.script_method
-
-############################################################################################################
-
-class RWKV_RNN(MyModule):
+class RWKV_RNN(torch.jit.ScriptModule):
     def __init__(self, args):
         super().__init__()
 
         self.args = args
         self.RUN_DEVICE = args.RUN_DEVICE
 
-        with torch.no_grad():
-            w = torch.load(args.MODEL_NAME + '.pth', map_location='cpu')
-            # refine weights and send to correct device
-            for k in w.keys():
-                block_id = int(k.split('.')[1]) if 'blocks.' in k else 0 # xác định block_id
-                if      '.time_' in k: w[k] = w[k].squeeze() # (A,1,B,1) => (A,B)
-                if '.time_decay' in k: w[k] = -torch.exp(w[k].float()) # e ^ negative = decay it's actually e ^ ( - e^ x )
-                else: w[k] = w[k].float()
-
-                w[k].requires_grad = False # chỉ inference, nên không cần gradient
-                if args.RUN_DEVICE == 'cuda' and k != 'emb.weight':
-                    w[k] = w[k].cuda() # emb lookup table stay in ram
-
-        # store weights in self.w
-        self.w = types.SimpleNamespace()
+        # Load params (trọng số) từ file vào vào bộ nhớ (biến w)
+        w = torch.load(args.MODEL_NAME + '.pth', map_location='cpu')
         for k in w.keys():
-            parts = k.split('.') # blocks.0.att.value.weight => ['block','0','att','value','weight']
+            if      '.time_' in k: w[k] = w[k].squeeze() # (A,1,B,1) => (A,B)
+            if '.time_decay' in k: w[k] = -torch.exp(w[k].float()) # e ^ negative = decay it's actually e ^ ( - e^ x )
+            else: w[k] = w[k].float() # convert to fp32 type
+
+            w[k].requires_grad = False # chỉ inference, nên không cần gradient
+            if args.RUN_DEVICE == 'cuda' and k != 'emb.weight':
+                w[k] = w[k].cuda()           # ^^ embedding lookup table stay in ram
+
+        # Gán trọng số vào tham số mô hình
+        self.w = types.SimpleNamespace()
+        self.w.blocks = {}
+        for k in w.keys():
+            parts = k.split('.') # Ví dụ k = "blocks.0.att.value.weight" => parts = ['block','0','att','value','weight']
             last = parts.pop() # => last = weight; parts = ['block','0','att','value']
-            here = self.w
-            for i, p in enumerate(parts): # mở rộng namespace
-                if p.isdigit():
+            here = self.w # độ sâu hiện tại của tham số mô hình
+            for i, p in enumerate(parts): # từng bước mở rộng namespace
+                if p.isdigit(): # tầng thứ p
                     p = int(p) # dùng [] vì here (w.blocks) là dict object {}
                     if p not in here: here[p] = types.SimpleNamespace()
                     here = here[p]
                 else: # dùng hasattr, setattr, getattr vì here là types.SimpleNamespace()
-                    if not hasattr(here, p):
-                        if p == "blocks": setattr(here, p, {})
-                        else: setattr(here, p, types.SimpleNamespace())
+                    if not hasattr(here, p): setattr(here, p, types.SimpleNamespace())
                     here = getattr(here, p)
-            setattr(here, last, w[k]) # gán giá trị vào name part cuối cùng
+            setattr(here, last, w[k]) # gán giá trị vào namespace cuối cùng => self.blocks[0].att.value.weight = w[k]
 
-        self.eval() # torch eval mode (not train mode)
+        self.eval() # set torch to inference mode
         gc.collect() # giải phóng ram
         torch.cuda.empty_cache() # giải phóng vram
 
-    def LN(self, x, w):
+    '''state[] để lưu trạng thái của rnn, bước chạy thứ i ghi lại 5 trạng thái: 
+    i+0 = ffn_xx : token của bước channel-mixing trước 
+    i+1 = att_xx : token của bước time-mixing trước
+    i+2 = att_aa : exp moving avg của kv 
+    i+3 = att_bb : exp moving avg của k
+    i+4 = att_pp : use pp to stove exponent of aa and bb
+    '''
+    def layer_norm(self, x, w):
         return F.layer_norm(x, (self.args.n_embd,), weight=w.weight, bias=w.bias)
 
-    # state[] i+0=ffn_xx i+1=att_xx i+2=att_aa i+3=att_bb i+4=att_pp
-    @MyFunction
-    def FF(self, x, state, i:int, time_mix_k, time_mix_r, kw, vw, rw):
-        ffn_xx = 5*i+0
+    @torch.jit.script_method
+    def channel_mixing(self, x, state, i:int, time_mix_k, time_mix_r, kw, vw, rw):
+        ffn_xx = 5*i+0 # feed-forward or channel mixing
         # token-shift with diff mixing factors for k and r
         xk = x * time_mix_k + state[ffn_xx] * (1 - time_mix_k)
         xr = x * time_mix_r + state[ffn_xx] * (1 - time_mix_r)
@@ -75,9 +64,8 @@ class RWKV_RNN(MyModule):
         k = torch.square(torch.relu(kw @ xk)) # square relu, primer paper
         return r * (vw @ k)
 
-    # state[] i+0=ffn_xx i+1=att_xx i+2=att_aa i+3=att_bb i+4=att_pp
-    @MyFunction
-    def SA(self, x, state, i:int, time_mix_k, time_mix_v, time_mix_r, time_first, time_decay, kw, vw, rw, ow):
+    @torch.jit.script_method
+    def time_mixing(self, x, state, i:int, time_mix_k, time_mix_v, time_mix_r, time_first, time_decay, kw, vw, rw, ow):
         att_xx = 5*i+1 # attention or time mixing
         # token-shift
         xk = x * time_mix_k + state[att_xx] * (1 - time_mix_k)
@@ -121,27 +109,26 @@ class RWKV_RNN(MyModule):
 
             if state == None: # khởi tạo trạng thái hệ thống
                 state = torch.zeros(args.n_layer * 5, args.n_embd, device=self.RUN_DEVICE)
-                for i in range(args.n_layer): state[5*i+4] -= 1e30 # att_pp = dương vô cực
+                for i in range(args.n_layer): state[5*i+4] -= 1e30 # state[att_pp] = dương vô cực
 
-            # Với mỗi tầng
-            for i in range(args.n_layer):
-                if i == 0: # áp dụng layer-norm-0 ở tầng đầu tiên để small-init-emb trick hoạt động
-                    x = self.LN(x, w.blocks[i].ln0)
-
-                # time-mixing
+            # Áp dụng layer-norm-0 ở tầng đầu tiên để small-init-emb trick hoạt động
+            x = self.layer_norm(x, w.blocks[0].ln0)
+            
+            for i in range(args.n_layer): # Với mỗi tầng áp dụng:
+                # 1/ time-mixing
                 att = w.blocks[i].att # trọng số của khối time-mixing
-                x = x + self.SA(self.LN(x, w.blocks[i].ln1), state, i, 
+                x = x + self.time_mixing(self.layer_norm(x, w.blocks[i].ln1), state, i, 
                     att.time_mix_k, att.time_mix_v, att.time_mix_r, att.time_first, att.time_decay, 
                     att.key.weight, att.value.weight, att.receptance.weight, att.output.weight)
                 
-                # channel-mixing
+                # 2/ channel-mixing
                 ffn = w.blocks[i].ffn # trọng số của khối channel-mixing
-                x = x + self.FF(self.LN(x, w.blocks[i].ln2), state, i, 
+                x = x + self.channel_mixing(self.layer_norm(x, w.blocks[i].ln2), state, i, 
                     ffn.time_mix_k, ffn.time_mix_r, 
                     ffn.key.weight, ffn.value.weight, ffn.receptance.weight)
-                
-            if preprocess_only: return state
 
-            x = self.LN(x, w.ln_out)
+            if preprocess_only: return state
+            # Cuối cùng là bộ phân lớp cho ra next token probabilities
+            x = self.layer_norm(x, w.ln_out)
             x = w.head.weight @ x
             return x.float(), state
--- run_f32.py
+++ run_f32.py
@@ -1,10 +1,10 @@
 import numpy as np
-import os, sys, types, time, gc
+import os, types, gc
 import torch
-from tknz import TOKENIZER
+from torch.nn import functional as F
 
-try: os.environ["CUDA_VISIBLE_DEVICES"] = sys.argv[1]
-except: pass
+from transformers import PreTrainedTokenizerFast
+TOKENIZER = PreTrainedTokenizerFast(tokenizer_file="20B_tokenizer.json")
 
 ##########################################################################################################
 # Step 1: set model & config (use v4 to run your trained-from-scratch models. v4 and v4neo are compatible)
@@ -14,13 +14,6 @@ args = types.SimpleNamespace()
 args.RUN_DEVICE = "cpu" # 'cuda' // 'cpu' (already fast)
 os.environ["RWKV_JIT_ON"] = '1' # '1' or '0'. very useful for GPU/CPU fp32
 os.environ["RWKV_RUN_DEVICE"] = args.RUN_DEVICE
-
-TOKEN_MODE = "pile"
-WORD_NAME = [
-    "20B_tokenizer.json",
-    "20B_tokenizer.json",
-]  # [vocab, vocab] for Pile model
-UNKNOWN_CHAR = None
 
 # wget https://huggingface.co/BlinkDL/rwkv-4-pile-169m/resolve/main/RWKV-4-Pile-169M-20220807-8023.pth
 args.MODEL_NAME = "RWKV-4-Pile-169M-20220807-8023"
@@ -44,7 +37,6 @@ LENGTH_PER_TRIAL = 222
 
 TEMPERATURE = 1.0
 top_p = 0.8
-top_p_newline = 0.9  # only used in TOKEN_MODE = char
 
 ########################################################################################################
 
@@ -54,64 +46,54 @@ from model_run_f32 import RWKV_RNN
 model = RWKV_RNN(args)
 
 out, _ = model.forward([187], None) # warm-up; token_id 187 => '\n'
-gc.collect(); torch.cuda.empty_cache()
+gc.collect(); torch.cuda.empty_cache() # free mem
 
-print(f'\nLoading tokenizer {WORD_NAME}...')
-tokenizer = TOKENIZER(WORD_NAME, UNKNOWN_CHAR=UNKNOWN_CHAR)
-assert TOKEN_MODE == "pile" and tokenizer.tokenizer.decode([187]) == '\n'
+
+def sample_logits(out, temperature=1.0, top_p_usual=None):
+    probs = F.softmax(out, dim=-1)
+    top_p = top_p_usual
+    probs = probs.cpu().numpy()
+    sorted_probs = np.sort(probs)[::-1]
+    cumulative_probs = np.cumsum(sorted_probs)
+    cutoff = float(sorted_probs[np.argmax(cumulative_probs > top_p)])
+    probs[probs < cutoff] = 0
+    if temperature != 1.0: probs = probs.pow(1.0 / temperature)
+    probs = probs / np.sum(probs)
+    return np.random.choice(a=len(probs), p=probs)
 
 ########################################################################################################
 
-ctx = tokenizer.tokenizer.encode(context)
-src_len = len(ctx)
-src_ctx = ctx.copy()
-
+src_ctx = TOKENIZER.encode(context) # context => token_ids
+src_len = len(src_ctx)
 print("\nYour prompt has " + str(src_len) + " tokens.")
 
 init_state = None
 init_out = None
-state = None
-out = None
 
-# TRIAL == 0:
+# Khởi tạo init_state, init_out cho lần chạy đầu tiên (TRIAL == 0)
 for i in range(src_len):
-    x = ctx[: i + 1]
+    x = src_ctx[: i + 1]
     if i == src_len - 1: init_out, init_state = model.forward(x, init_state)
     else: init_state = model.forward(x, init_state, preprocess_only=True)
-gc.collect()
-torch.cuda.empty_cache()
+gc.collect(); torch.cuda.empty_cache() # free mem
 
 for TRIAL in range(NUM_TRIALS):
     print(f'\n\n--[ trial {TRIAL} ]------------------------------------------------\n' + context, end="")
 
-    time_ref = time.time_ns()
     ctx = src_ctx.copy()
     out_last = src_len
-    for i in range(src_len, src_len + LENGTH_PER_TRIAL):
-        x = ctx[: i + 1]
-        x = x[-args.ctx_len:]
+    out = init_out.clone()
+    state = init_state.clone()
 
-        if i == src_len:
-            out = init_out.clone()
-            state = init_state.clone()
-        else:
-            out, state = model.forward(x, state)
-        if TOKEN_MODE == "pile":
-            out[0] = -999999999  # disable <|endoftext|>
+    for i in range(src_len, src_len + LENGTH_PER_TRIAL): # sinh thêm LENGTH_PER_TRIAL
+        input_token_ids = ctx[: i + 1][-args.ctx_len:] # lấy args.ctx_len last tokens
+        out, state = model.forward(input_token_ids, state)
+        out[0] = -999999999  # disable <|endoftext|>
 
-        ttt = tokenizer.sample_logits(
-            out, x, args.ctx_len,
-            temperature=TEMPERATURE,
-            top_p_usual=top_p,
-            top_p_newline=top_p_newline,
-        )
-        ctx += [ttt]
+        next_token_id = sample_logits(out, TEMPERATURE, top_p) # lấy mẫu ngẫu nhiên token tiếp theo
+        ctx.append(next_token_id)
 
-        if tokenizer.charMode:
-            char = tokenizer.itos[ttt]
-            print(char, end="", flush=True)
-        else:
-            char = tokenizer.tokenizer.decode(ctx[out_last:])
-            if '\ufffd' not in char: # is valid utf8 string?
-                print(char, end="", flush=True)
-                out_last = i+1
+        token = TOKENIZER.decode(ctx[out_last:])
+        if '\ufffd' not in token: # is valid utf8 string? (bỏ qua invalid token)
+            print(token, end="", flush=True)
+            out_last = i+1
