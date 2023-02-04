81ea7a83c411ab83c5f6c1d4f6919f51e80047f0
Tien Dung <dungtn@gmail.com>
2023-02-04 08:43:44 +0700

    remove unused file formats

--- rwkv-v4neo/dataset.py
+++ rwkv-v4neo/dataset.py
@@ -7,64 +7,22 @@ import numpy as np
 import torch
 from torch.utils.data import Dataset
 from pytorch_lightning.utilities import rank_zero_info
-from binidx import MMapIndexedDataset
 from utils import MaybeIsPrime
 
 
 class MyDataset(Dataset):
     def __init__(self, args):
         self.args = args
-
-        if args.data_type == "binidx":
-            self.vocab_size = args.vocab_size
-            rank_zero_info(f"Current vocab size = {self.vocab_size} (make sure it's correct)")
-            if args.data_file.endswith('/'):
-                d_all = [] # tìm trong dir các file .idx
-                for p in os.listdir(args.data_file):
-                    if p.endswith(".idx"): d_all += [p[:-4]]
-                d_all.sort()
-                rank_zero_info(d_all)
-                exit(0)
-            else:
-                self.data = MMapIndexedDataset(args.data_file)
-                self.data_size = len(self.data._bin_buffer) // 2
-                rank_zero_info(f"Data has {self.data_size} tokens.")
-
-            if args.my_pile_stage > 0:
-                # assert self.data_size == 332115325534 and self.vocab_size == 50277
-                self.samples_per_epoch = args.epoch_steps * args.real_bsz
-                assert self.samples_per_epoch == 40320
-                rank_zero_info(f"########## Pile 20b-tokenized stage {args.my_pile_stage} ##########")
-                dataset_slot = self.data_size // args.ctx_len
-                assert MaybeIsPrime(args.magic_prime)
-                assert args.magic_prime % 3 == 2
-                assert args.magic_prime / dataset_slot > 0.99 and args.magic_prime / dataset_slot <= 1
-
-        elif args.data_type == "numpy":
-            self.data = np.load(args.data_file).astype("int")
-            self.vocab_size = args.vocab_size
-            rank_zero_info("Current vocab size =", self.vocab_size, "(make sure it's correct)")
-            self.data_size = len(self.data)
-            rank_zero_info(f"Data has {self.data_size} tokens.")
-
-        elif args.data_type == "uint16":
-            self.data = np.fromfile(args.data_file, dtype=np.uint16).astype("int32").reshape(-1, args.my_sample_len)
-            self.vocab_size = args.vocab_size
-            rank_zero_info("Current vocab size =", self.vocab_size, "(make sure it's correct)")
-            self.data_size = self.data.shape[0]
-            rank_zero_info(f"Data has {self.data_size} samples.")
-
+        if args.data_type == "dummy":
+            rank_zero_info("Building dummy data...")
+            self.data = ""
+            for i in range(100000):
+                aa = (i) % 10000
+                bb = (i * i) % 10000
+                cc = aa + bb
+                self.data += f".{aa}+{bb}={cc}."
         else:
-            if args.data_type == "dummy":
-                rank_zero_info("Building dummy data...")
-                self.data = ""
-                for i in range(100000):
-                    aa = (i) % 10000
-                    bb = (i * i) % 10000
-                    cc = aa + bb
-                    self.data += f".{aa}+{bb}={cc}."
-            else:
-                self.data = open(args.data_file, "r", encoding=args.data_type).read()
+            self.data = open(args.data_file, "r", encoding=args.data_type).read()
 
             rank_zero_info("Building token list...")
             unique = sorted(list(set(self.data)))
@@ -74,7 +32,7 @@ class MyDataset(Dataset):
             for u in unique:
                 xxObj[xx] = u
                 xx += 1
-            with open(f"{args.proj_dir}/vocab.json", "w", encoding="utf-16le") as vocab_file:
+            with open(f"{args.proj_dir}/vocab.json", "w", encoding="utf-8") as vocab_file:
                 vocab_file.write(json.dumps(xxObj, ensure_ascii=False))
             self.data_size = len(self.data)
             rank_zero_info(f"Data has {self.data_size} tokens, {self.vocab_size} vocab size.")
